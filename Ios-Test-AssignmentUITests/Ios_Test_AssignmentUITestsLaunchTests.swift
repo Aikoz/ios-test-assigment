8cf14206ee88cd802c085419ac9bf23f1bb805a8
Aikoz <aikoz_1@live.com>
2024-10-09 10:46:48 -0600

    init commit

---
+++ Ios-Test-AssignmentUITests/Ios_Test_AssignmentUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Ios_Test_AssignmentUITestsLaunchTests.swift
+//  Ios-Test-AssignmentUITests
+//
+//  Created by José Luis Florencio Ortiz on 09/10/24.
+//
+
+import XCTest
+
+final class Ios_Test_AssignmentUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
